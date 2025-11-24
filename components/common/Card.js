c3945b6b3a939d23ee6efb209cc0c7023a5c5fb8
Akila Nirmal <akilanirmalzz4352@gmail.com>
2025-11-24 14:54:37 +0530

    cards

--- components/common/Card.js
+++ components/common/Card.js
@@ -2,35 +2,49 @@ import Link from "next/link"
 import { TitleSm } from "./Title"
 import { HiOutlineArrowRight } from "react-icons/hi"
 
-export const Card = ({ data, caption, show, path }) => {
-  return (
-    <>
-      <div className='card'>
-        <div className='card-img'>
-          <img src={data.cover} alt={data.title} />
-        </div>
-        <div className='card-details'>
-          <Link href={`${path}/${data.id}`} className='title-link'>
-            <TitleSm title={data.title} />
-          </Link>
-          {caption && (
-            <Link href={`${path}/${data.id}`}>
-              {caption} <HiOutlineArrowRight className='link-icon' />
-            </Link>
-          )}
-          <div className='flex'>
-            <span> {data.catgeory} </span> {data.date && <span> / {data.date}</span>}
-          </div>
+export const Card = ({ data = {}, caption, show, path }) => {
+	// safe extraction with defaults
+	const id = data?.id
+	const href = path && id ? `${path}/${id}` : "#"
+	const title = data?.title ?? ""
+	const cover = data?.cover ?? ""
+	// support both 'category' and misspelled 'catgeory'
+	const category = data?.category ?? data?.catgeory ?? ""
+	const date = data?.date ?? ""
+	const desc = Array.isArray(data?.desc) ? data.desc : []
 
-          {show && (
-            <ul>
-              {data.desc.map((text, i) => (
-                <li key={i}> - {text.text}</li>
-              ))}
-            </ul>
-          )}
-        </div>
-      </div>
-    </>
-  )
+	return (
+		<>
+			<div className='card'>
+				<div className='card-img'>
+					{cover ? (
+						<img src={cover} alt={title || "cover image"} />
+					) : (
+						<img src='/placeholder.png' alt={title || "placeholder"} />
+					)}
+				</div>
+				<div className='card-details'>
+					<Link href={href} className='title-link'>
+						<TitleSm title={title} />
+					</Link>
+					{caption && (
+						<Link href={href}>
+							{caption} <HiOutlineArrowRight className='link-icon' />
+						</Link>
+					)}
+					<div className='flex'>
+						<span>{category}</span> {date && <span> / {date}</span>}
+					</div>
+
+					{show && desc.length > 0 && (
+						<ul>
+							{desc.map((text, i) => (
+								<li key={i}> - {text?.text ?? text}</li>
+							))}
+						</ul>
+					)}
+				</div>
+			</div>
+		</>
+	)
 }
