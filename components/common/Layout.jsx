d1edfe0a3682d9eb9818facbf443175a2470c176
Akila Nirmal <akilanirmalzz4352@gmail.com>
2025-12-30 15:31:02 +0530

    block remove

--- components/common/Layout.jsx
+++ components/common/Layout.jsx
@@ -2,27 +2,16 @@ import Footer from "./Footer"
 import Header from "./Header"
 import { useRouter } from "next/router"
 
-
 const Layout = (props) => {
-  const router = useRouter();
-  const isAdmin = router.pathname.startsWith("/admin");
-  // TEMPORARY: Block site due to payment issue
-  const showOverlay = true;
+  const router = useRouter()
+  const isAdmin = router.pathname.startsWith("/admin")
   return (
     <>
-      {showOverlay && (
-        <div className="payment-block-overlay">
-          <div className="payment-block-message">
-            <h1>Site Closed</h1>
-            <p>Access is temporarily disabled.<br/>Development payment not received.</p>
-          </div>
-        </div>
-      )}
       <Header />
       <main>{props.children}</main>
       {!isAdmin && <Footer />}
     </>
-  );
+  )
 }
 
 export default Layout
